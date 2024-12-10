1d2ecfbd22aa6bccdbf669c2b3ee6a32c8868ef6
CarmenRodriguezCardenal22 <carmen.rodriguezcardenal@iesvalleinclan.es>
2024-12-10 14:40:53 +0100

    A

--- React/actividad_prueba/src/App.js
+++ React/actividad_prueba/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import HelloComponent from './components/HelloComponent';
 import React,{Component} from 'react';
+import InputComponent from './components/InputComponent';
 
 class App extends Component {
   constructor(){
@@ -21,7 +22,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <HelloComponent nombre={this.state.name}></HelloComponent>
-          <input value={this.state.name} onChange={this.changeName}></input>
+          <InputComponent nombre={this.state.name} cambiarNombre={this.changeName}></InputComponent>
         </header>
       </div>
     );
