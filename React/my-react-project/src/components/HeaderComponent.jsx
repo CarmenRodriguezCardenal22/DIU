28637453652dc7cc8f13558189e9958baf09f5b0
CarmenRodriguezCardenal22 <carmen.rodriguezcardenal@iesvalleinclan.es>
2024-12-11 12:45:14 +0100

    A

---
+++ React/my-react-project/src/components/HeaderComponent.jsx
@@ -0,0 +1,24 @@
+function HeaderComponent() {
+    return(
+        <header>
+            <h1>Bienvenido</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Blog</a>
+                    </li>
+                    <li>
+                        <a href="#">News</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact Us</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+export default HeaderComponent;
